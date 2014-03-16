bca9dd5795a2fcfa6ea3e86c0e2d425ec668463d
samuelguegon89 <samuelguegongithub@gmail.com>
2014-03-16 20:26:12 +0100

    Tercera actualización y final del programa

--- tussam.py
+++ tussam.py
@@ -2,10 +2,7 @@
 
 from lxml import etree
 from suds.client import Client
-a=1
-
-while a!=0:
-	
+try:
 	linea=raw_input("Pon un numero de linea: ")
 	cliente = Client('http://www.infobustussam.com:9001/services/dinamica.asmx?wsdl', retxml=True)
 	respuesta=cliente.service.GetStatusLinea(linea)
@@ -18,8 +15,16 @@ while a!=0:
 	frecuencia=obtener2.find(ns+nb+ns+"frec_bien")
 	graves=obtener2.find(ns+nb+ns+"graves")
 	
-	print "El numero de coches activos en la linea",linea,"son:",activos.text,
-	print "\n""El numero de coches que van bien de frecuencia es:",frecuencia.text,
-	print "\n""El numero de incidencias graves son:",graves.text,"\n"
+	print "------------------------------------------------------"
+	print "Los datos de TUSSAM son los siguientes:"
+	print "------------------------------------------------------"
+	print "El número de coches activos en la linea",linea,"son:",activos.text,
+	print "\n""El número de coches que van bien de frecuencia es:",frecuencia.text,
+	print "\n""El número de incidencias graves son:",graves.text,
+	print "\n""------------------------------------------------------"
 
+except:
+	print "----------------------------"
+	print "La linea número",linea,"no existe."
+	print "----------------------------"
 
